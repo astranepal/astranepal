9497f571ec8a25d9f6dee62d7c856a0f0fca8149
astranepal <tejushacharya7@gmail.com>
2026-09-24 14:31:03 +0545

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,3 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig={images:{remotePatterns:[{protocol:"https",hostname:"images.unsplash.com"},{protocol:"https",hostname:"yggklzrakhyjyzjpzbzx.supabase.co"}]}};
+export default nextConfig;
