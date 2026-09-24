82b93d3b8a5dba9c8f589f167a146e2351f11785
astranepal <tejushacharya7@gmail.com>
2026-09-24 14:33:40 +0545

    Add app/account/actions.ts

---
+++ app/account/actions.ts
@@ -0,0 +1,1 @@
+"use server"; import {createClient} from "@/lib/supabase/server"; import {redirect} from "next/navigation"; export async function signOut(){const s=await createClient();await s.auth.signOut();redirect("/");}
